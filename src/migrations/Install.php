554d98c264f88290f7771c555658d52ed7c1917f
Joachim Schole <jschole@fork.de>
2018-12-10 17:25:20 +0100

    require redactor and add an install migration that creates a redactor and an embeds field

---
+++ src/migrations/Install.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace fork\embeds\migrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\fields\Matrix;
+use craft\records\FieldGroup;
+use craft\redactor\Field;
+
+/**
+ * Install migration.
+ */
+class Install extends Migration
+{
+    /**
+     * @inheritdoc
+     * @throws \Throwable
+     */
+    public function safeUp()
+    {
+        // See if Redactor is installed
+        if (!Craft::$app->plugins->isPluginInstalled('redactor')) {
+            // If not, try to install it
+            Craft::$app->plugins->installPlugin('redactor');
+        }
+        // Find the 'Common' field group
+        $group = FieldGroup::findOne(['name' => 'Common']);
+
+        // Build a Redactor field
+        $embedsCopy = new Field([
+            "groupId" => $group->id,
+            "name" => "Embeds Copy",
+            "handle" => "embedsCopy",
+            "instructions" => "Redactor copytext field for the Embeds plugin"
+        ]);
+        // Attempt to save the field
+        if (Craft::$app->fields->saveField($embedsCopy)) {
+            // Create a minimal matrix field with the required handle
+            $embedsMatrix = new Matrix([
+                "groupId" => $group->id,
+                "name" => "Embeds",
+                "handle" => "embeds",
+                "instructions" => "Embeds for the Redactor content"
+            ]);
+            // Attempt to save the field
+            return (Craft::$app->fields->saveField($embedsMatrix));
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        //-- Don't delete anything, too risky! --//
+        // Find the embeds matrix
+        /** @var Matrix $embedsMatrix */
+        //$embedsMatrix = Craft::$app->fields->getFieldByHandle("embeds");
+        // If found, Delete it
+        //if ($embedsMatrix) {
+        //    return (Craft::$app->fields->deleteFieldById($embedsMatrix->id));
+        //} else {
+        //    return true;
+        //}
+        return true;
+    }
+}
